73a6957aff9fa200d0f350f802f5ad06328cb454
kennywasneverhere <kennethgeorgewilson534@gmail.com>
2025-07-26 14:12:54 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~"malvin~E7cQHTSJ#f2tebgj8uyes1IAMSCU2MZUdXLmAxf4BF5eSfhHYvOo"
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
